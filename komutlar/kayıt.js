83e496ed5208f38cfea7dbf31d7557e36ee76412
xKennyPvp <54753390+xKennyPvp@users.noreply.github.com>
2019-08-31 23:55:27 +0300

    Add files via upload

---
+++ komutlar/kayıt.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js'); 
+
+exports.run = async (client, message, args) => { 
+ 
+  let user = message.member 
+  let guild = message.guild 
+  
+  let rol = guild.roles.find('name', 'Uye' , '「👨」Aboneler')
+  let isim = args[0] 
+  let yas = args[1] 
+  
+  if (!isim) return message.channel.send(`İsmini girmelisin.`) 
+  if (!yas) return message.channel.send(`Yaşını girmelisin.`) 
+  
+  user.setNickname(`${isim} | ${yas}`) 
+  user.addRole(rol) 
+  message.channel.send(`${message.author} Sunucuya Kaydoldun !`) 
+}; 
+
+exports.conf = { 
+  enabled: true, 
+  guildOnly: true, 
+  aliases: [], 
+  permLevel: 0 
+} 
+
+exports.help = { 
+  name: 'kayıt', 
+  description: "Sunucuya kaydolmaya ne dersin ?", 
+  usage: 'kayıt isim yaş' 
+}
